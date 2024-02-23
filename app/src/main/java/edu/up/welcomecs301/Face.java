a0ad970cc56d05430dd4da95076ca9fd9f78690c
Carolyn <sousa24@up.edu>
2024-02-23 00:02:18 -0800

    HW Part B

--- app/src/main/java/edu/up/welcomecs301/Face.java
+++ app/src/main/java/edu/up/welcomecs301/Face.java
@@ -3,6 +3,9 @@
 package edu.up.welcomecs301;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import java.util.Random;
 
 public class Face {
@@ -10,6 +13,8 @@ public class Face {
     private int eyeColor;
     private int hairColor;
     private int hairStyle;
+
+    private static final String[] HAIR_STYLES = {"Short", "Medium", "Long"};
 
     public Face() {
         randomize();
@@ -20,10 +25,75 @@ public class Face {
         skinColor = random.nextInt(0xFFFFFF + 1);
         eyeColor = random.nextInt(0xFFFFFF + 1);
         hairColor = random.nextInt(0xFFFFFF + 1);
-        hairStyle = random.nextInt(5);
+        hairStyle = random.nextInt(HAIR_STYLES.length);
     }
 
-    public void onDraw(Canvas canvas) {
+    public int getSkinColor() {
+        return skinColor;
+    }
 
+    public void setSkinColor(int skinColor) {
+        this.skinColor = skinColor;
+    }
+
+    public int getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(int eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public int getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(int hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public int getHairStyle() {
+        return hairStyle;
+    }
+
+    public void setHairStyle(int hairStyle) {
+        this.hairStyle = hairStyle;
+    }
+
+    public static String[] getHairStyles() {
+        return HAIR_STYLES;
+    }
+
+
+    public void onDraw(Canvas canvas, int viewWidth, int viewHeight) {
+        // Draw hair
+        Paint hairPaint = new Paint();
+        hairPaint.setColor(hairColor);
+
+        // Calculate hair position and size
+        int hairTop = viewHeight / 2 - 200;
+        int hairBottom = viewHeight / 2 + 100;
+        int hairWidth = 350;
+
+        if (hairStyle == 1) {
+            hairBottom = viewHeight / 2 + 150;
+        } else if (hairStyle == 2) {
+            hairBottom = viewHeight / 2 + 175;
+        }
+
+        // Draw hair rectangle
+        canvas.drawRect(new RectF(viewWidth / 2 - hairWidth / 2, hairTop,
+                viewWidth / 2 + hairWidth / 2, hairBottom), hairPaint);
+
+        // Draw face shape
+        Paint facePaint = new Paint();
+        facePaint.setColor(skinColor);
+        canvas.drawCircle(viewWidth / 2, viewHeight / 2, 150, facePaint);
+
+        // Draw eyes
+        Paint eyePaint = new Paint();
+        eyePaint.setColor(eyeColor);
+        canvas.drawCircle(viewWidth / 2 - 75, viewHeight / 2 - 50, 15, eyePaint);
+        canvas.drawCircle(viewWidth / 2 + 75, viewHeight / 2 - 50, 15, eyePaint);
     }
 }
--- app/src/main/java/edu/up/welcomecs301/MainActivity.java
+++ app/src/main/java/edu/up/welcomecs301/MainActivity.java
@@ -2,15 +2,123 @@
 
 package edu.up.welcomecs301;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Face face;
+    private Spinner hairStyleSpinner;
+    private RadioGroup featureRadioGroup;
+    private SeekBar redSeekBar, greenSeekBar, blueSeekBar;
+    private Button randomFaceButton;
+    private FaceView faceSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+        setListeners();
+
+        face = new Face();
+        updateViewsFromFace();
     }
+
+    private void initializeViews() {
+        hairStyleSpinner = findViewById(R.id.hairStyleSpinner);
+        featureRadioGroup = findViewById(R.id.featureRadioGroup);
+        redSeekBar = findViewById(R.id.redSeekBar);
+        greenSeekBar = findViewById(R.id.greenSeekBar);
+        blueSeekBar = findViewById(R.id.blueSeekBar);
+        randomFaceButton = findViewById(R.id.randomFaceButton);
+        faceSurfaceView = findViewById(R.id.faceSurfaceView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Face.getHairStyles());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hairStyleSpinner.setAdapter(adapter);
+    }
+
+    //onItemSelected info
+    //https://stackoverflow.com/questions/20151414/how-can-i-use-onitemselected-in-android
+    private void setListeners() {
+        hairStyleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                face.setHairStyle(position);
+                updateViewsFromFace();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        featureRadioGroup.setOnCheckedChangeListener((group, checkedId) -> updateSeekBarProgress());
+
+        redSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+        greenSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+        blueSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+
+        randomFaceButton.setOnClickListener(v -> {
+            face.randomize();
+            updateViewsFromFace();
+        });
+    }
+
+    private void updateSeekBarProgress() {
+        int color;
+        if (featureRadioGroup.getCheckedRadioButtonId() == R.id.hairRadioButton) {
+            color = face.getHairColor();
+        } else if (featureRadioGroup.getCheckedRadioButtonId() == R.id.eyesRadioButton) {
+            color = face.getEyeColor();
+        } else {
+            color = face.getSkinColor();
+        }
+        redSeekBar.setProgress(Color.red(color));
+        greenSeekBar.setProgress(Color.green(color));
+        blueSeekBar.setProgress(Color.blue(color));
+    }
+
+    private void updateViewsFromFace() {
+        hairStyleSpinner.setSelection(face.getHairStyle());
+
+        updateSeekBarProgress();
+        faceSurfaceView.updateFace(face);
+    }
+
+    private final SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            int color = Color.rgb(redSeekBar.getProgress(), greenSeekBar.getProgress(), blueSeekBar.getProgress());
+            if (featureRadioGroup.getCheckedRadioButtonId() == R.id.hairRadioButton) {
+                face.setHairColor(color);
+            } else if (featureRadioGroup.getCheckedRadioButtonId() == R.id.eyesRadioButton) {
+                face.setEyeColor(color);
+            } else {
+                face.setSkinColor(color);
+            }
+            updateViewsFromFace();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {}
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {}
+
+    };
 }
